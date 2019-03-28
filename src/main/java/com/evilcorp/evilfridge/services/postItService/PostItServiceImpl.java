03b24bb47e6af63e320759f08e17931af6320521
RBKoronczi <rbkoronczi@gmail.com>
2019-03-28 17:11:51 +0100

    Created PostService structure

--- src/main/java/com/evilcorp/evilfridge/services/postItService/PostItServiceImpl.java
+++ src/main/java/com/evilcorp/evilfridge/services/postItService/PostItServiceImpl.java
@@ -1,8 +1,11 @@
 package com.evilcorp.evilfridge.services.postItService;
 
+import com.evilcorp.evilfridge.models.PostIt;
 import com.evilcorp.evilfridge.repositories.PostItRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PostItServiceImpl implements PostItService{
@@ -12,5 +15,30 @@ public class PostItServiceImpl implements PostItService{
   @Autowired
   public PostItServiceImpl(PostItRepository postItRepository) {
     this.postItRepository = postItRepository;
+  }
+
+  @Override
+  public List<PostIt> getPostItsOfUser(Long userId) {
+    return null;
+  }
+
+  @Override
+  public List<PostIt> getPostItsOfFridge(Long fridgeId) {
+    return null;
+  }
+
+  @Override
+  public List<PostIt> getPostItsOfUserAndFridge(Long userId, Long fridgeId) {
+    return null;
+  }
+
+  @Override
+  public boolean savePostIt(PostIt postIt) {
+    return false;
+  }
+
+  @Override
+  public boolean deletePostIt(Long postItId) {
+    return false;
   }
 }
