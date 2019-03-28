0212f6b1caf8370f633b8561dbf3277061dd5722
RBKoronczi <rbkoronczi@gmail.com>
2019-03-28 21:45:50 +0100

    Created endpoints for Fridge Controller, and corresponding services

--- src/main/java/com/evilcorp/evilfridge/repositories/FridgeRepository.java
+++ src/main/java/com/evilcorp/evilfridge/repositories/FridgeRepository.java
@@ -4,6 +4,9 @@ import com.evilcorp.evilfridge.models.Fridge;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FridgeRepository extends JpaRepository<Fridge, Long> {
+    public List<Fridge> findByUsersContain_Id(Long id);
 }
