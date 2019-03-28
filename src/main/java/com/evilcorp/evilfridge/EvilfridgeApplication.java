22287edc81c011308f6306c84acf2aeab6d4f1ab
Szőke Tamás <tamas.szoke.88@gmail.com>
2019-03-28 11:57:24 +0100

    Initializing project

---
+++ src/main/java/com/evilcorp/evilfridge/EvilfridgeApplication.java
@@ -0,0 +1,13 @@
+package com.evilcorp.evilfridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvilfridgeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EvilfridgeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/evilcorp/evilfridge/EvilfridgeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.evilcorp.evilfridge;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EvilfridgeApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
